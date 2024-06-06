ccf1c99c3b2aecf9b89224a3bde53c64e56a06bf
Vaishali Gupta <vaishaligupta0205@gmail.com>
2024-06-06 06:35:52 -0700

    adding noreferrer to target blank

--- src/landingComponent.js
+++ src/landingComponent.js
@@ -28,7 +28,7 @@ function LandingComponent() {
         <NavbarBrand href="/">
          Portfolio
        </NavbarBrand>
-       <NavbarText><a href='mailto:vaishaligupta0205@gmail.com' target='_blank'>Contact Me</a></NavbarText>
+       <NavbarText><a href='mailto:vaishaligupta0205@gmail.com' target='_blank' rel='noreferrer'>Contact Me</a></NavbarText>
       </Navbar>
 
 
@@ -73,7 +73,7 @@ function LandingComponent() {
                   <CardText>
                     Using this website user can sell and buy car. They can check the car related details.
                   </CardText>
-                  <Button color="info" href="https://carhub-shop.netlify.app/" target="_blank">
+                  <Button color="info" href="https://carhub-shop.netlify.app/" target="_blank" rel='noreferrer'>
                     Demo
                   </Button>
                 </CardBody>
@@ -92,7 +92,7 @@ function LandingComponent() {
                   <CardText>
                     This website is in retail Domain of fancy shoe. User can buy the shoe online. 
                   </CardText>
-                  <Button color="info" href="https://fancy-shoe-store.netlify.app" target="_blank">
+                  <Button color="info" href="https://fancy-shoe-store.netlify.app" target="_blank" rel='noreferrer'>
                     Demo
                   </Button>
                 </CardBody>
@@ -121,9 +121,9 @@ function LandingComponent() {
       </Row>
       <Row>
         <Col id="social">
-          <a href='https://www.linkedin.com/in/vaishali-gupta/' target='_blank' title='LinkedIn Profile'><img src={linkedin} ></img></a>
-          <a href='https://github.com/vaishali-gupta/' target='_blank' title='Github Profile'> <img src={github}></img></a>
-          <a href='https://drive.google.com/file/d/1kQViIKjImTk3LyDhiC6n_cWDcXXeQuzT/view?usp=sharing' target='_blank>' title='Download Resume'><img src={resume}></img></a>
+          <a href='https://www.linkedin.com/in/vaishali-gupta/' target='_blank' rel='noreferrer' title='LinkedIn Profile'><img src={linkedin} ></img></a>
+          <a href='https://github.com/vaishali-gupta/' target='_blank' rel='noreferrer' title='Github Profile'> <img src={github}></img></a>
+          <a href='https://drive.google.com/file/d/1kQViIKjImTk3LyDhiC6n_cWDcXXeQuzT/view?usp=sharing' target='_blank' rel='noreferrer' title='Download Resume'><img src={resume}></img></a>
         </Col>
       </Row>
     </div>
